3a31089ec25fe07da02c1a64bb4dc5f12113dc0a
Wenhao Li <69703322+wenhaoli-xmu@users.noreply.github.com>
2025-12-20 21:17:26 +0800

    Update greedy_generation.py

--- spotlight/modifiers/greedy_generation.py
+++ spotlight/modifiers/greedy_generation.py
@@ -22,6 +22,9 @@ class Greedy(Modifier):
     @torch.no_grad()
     def generate(self, input_ids, max_new_tokens=128, eos_token_id=[2]):
 
+        if isinstance(input_ids, list):
+            input_ids = torch.tensor(input_ids, dtype=torch.int64)[None, :]
+
         if input_ids.ndim == 3:
             input_ids = input_ids.flatten(0,1)
 
@@ -31,7 +34,7 @@ class Greedy(Modifier):
 
         # prefilling
         output = self.model(input_ids=input_ids)
-        logits, kv_cache = output.logits, output.past_key_values
+        logits, past_key_values = output.logits, output.past_key_values
         new_tok = logits.argmax(dim=-1)
         new_ids = [new_tok]
 
@@ -39,15 +42,13 @@ class Greedy(Modifier):
         new_tok = input_ids[:, -1:]
         new_ids = []
 
-        
         while len(new_ids) < max_new_tokens:
-            output = self.model(input_ids=new_tok, kv_cache=kv_cache)
-            logits, kv_cache = output.logits, output.past_key_values
+            output = self.model(input_ids=new_tok, past_key_values=past_key_values)
+            logits, past_key_values = output.logits, output.past_key_values
 
             new_tok = logits.argmax(dim=-1)
             if new_tok.ravel().item() in eos_token_id: break
             new_ids.append(new_tok.ravel().item())
 
-        self.model.reset()
         new_ids = torch.tensor(new_ids, dtype=input_ids.dtype, device=input_ids.device)[None, :]
         return torch.cat([input_ids, new_ids], dim=-1)
