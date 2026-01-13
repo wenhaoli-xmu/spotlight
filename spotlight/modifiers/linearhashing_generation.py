b0ee8207b28cfe2be3319079602b81bde84ed373
Wenhao Li <69703322+wenhaoli-xmu@users.noreply.github.com>
2026-01-13 14:17:13 +0800

    Update linearhashing_generation.py

--- spotlight/modifiers/linearhashing_generation.py
+++ spotlight/modifiers/linearhashing_generation.py
@@ -490,33 +490,29 @@ class LinearHashing(Modifier):
     def disable_benchmark_mode(self):
         return self.model.decoder.disable_benchmark_mode()
 
-
+    
     @torch.no_grad()
     def generate(self, input_ids, max_new_tokens=128, eos_token_id=[2]):
 
         if input_ids.ndim == 3:
             input_ids = input_ids.flatten(0,1)
 
-        # put the tensor on to the model's device
         device = next(iter(self.model.parameters())).device
         input_ids = input_ids.to(device)
 
         # prefilling
         output = self.model(input_ids=input_ids)
         logits, kv_cache = output.logits, output.past_key_values
-        new_tok = logits.argmax(dim=-1)
-        new_ids = [new_tok]
-
-        # generation
-        new_tok = input_ids[:, -1:]
+        new_tok = logits[:, -1:].argmax(dim=-1)
         new_ids = []
-        while len(new_ids) < max_new_tokens:
-            output = self.model(input_ids=new_tok, kv_cache=kv_cache)
-            logits, kv_cache = output.logits, output.past_key_values
 
-            new_tok = logits.argmax(dim=-1)
+        while len(new_ids) < max_new_tokens:
             if new_tok.ravel().item() in eos_token_id: break
             new_ids.append(new_tok.ravel().item())
+            
+            output = self.model(input_ids=new_tok, kv_cache=kv_cache)
+            logits, kv_cache = output.logits, output.past_key_values
+            new_tok = logits.argmax(dim=-1)
 
         self.model.reset()
         new_ids = torch.tensor(new_ids, dtype=input_ids.dtype, device=input_ids.device)[None, :]
